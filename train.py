cc11fb838ee9a0fa66e03073af32b3a82478a904
yangzonglin <806765278@qq.com>
2018-06-12 03:51:12 +0800

    1、新增calcu_cost的说明文档。 2、避免出现秩为1的数组。 3、新增一些注释。

--- train.py
+++ train.py
@@ -5,6 +5,12 @@ from my_link_based_model import MyLinkBasedModel
 
 
 def calcu_cost(Y, y):
+    """
+    计算整个训练集的cost
+    :param Y: class Variable object, model output, shape = (1, m)
+    :param y: class Variable object, actual value, shape = (1, m)
+    :return: cost of entire training set
+    """
     Y, y = Y.data, y.data
     loss = -(y * np.log(Y) + (1-y) * np.log(1-Y))
     # print(type(loss))
@@ -14,9 +20,11 @@ def calcu_cost(Y, y):
 
 model = MyLinkBasedModel()
 X = Variable(np.array([[1, 2, 3], [2, 1, 3], [2, 2, 1]], dtype=np.float32)).T
-y = Variable(np.array([0, 1, 0], dtype=np.float32))
+y = Variable(np.array([0, 1, 0], dtype=np.float32)).reshape(1, 3)
 iter_num = 100
 LR = 0.1
+# BGD，一轮迭代，只更新一次模型参数
+# BGD，只有一个batch，故一轮迭代只有一次forward and backward pass
 for i in range(iter_num):
     Y = model(X)
     print(calcu_cost(Y, y))
